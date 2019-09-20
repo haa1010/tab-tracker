22b5f2fbc18e401841ad081cfe1b0f889fcdc33e
haa1010 <hang40338@gmail.com>
2019-09-20 15:36:13 +0700

    setup of the express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+
+        "commonjs": true,
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {}
+};
